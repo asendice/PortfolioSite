294fccacab4b177d92b13e90c0663d02ca11f3ab
Dylan Travis <dylan.travis23@outlook.com>
2020-02-29 00:32:17 -0800

    Week 1 Portfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(function() {
+    $('#modal-button1').click(function(){
+        $("#project1-modal").modal("show");
+    });
+
+    $('#modal-button2').click(function(){
+        $("#project2-modal").modal("show");
+    });
+
+    $('#modal-button3').click(function(){
+        $("#project3-modal").modal("show");
+    });
+});
